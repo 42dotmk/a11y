d7b7a242ff41c0694975dab71577d5ed6883f3f2
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-11-26 19:18:24 +0100

    Dockerize solution

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
 import markdownIntegration from '@astropub/md'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,12 @@ export default defineConfig({
   output: 'hybrid',
   integrations: [
     markdownIntegration(),
-    mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+  adapter: node({ mode: 'standalone' }),
 })
